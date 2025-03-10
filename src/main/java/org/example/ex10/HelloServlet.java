cd720842479db8105415db6a34b5f65888758ecb
hsu-git <hsu080206@gmail.com>
2025-03-10 12:05:39 +0900

    nickname (dotenv, doget)

--- src/main/java/org/example/ex10/HelloServlet.java
+++ src/main/java/org/example/ex10/HelloServlet.java
@@ -2,15 +2,21 @@ package org.example.ex10;
 
 import java.io.*;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
-    private String message;
+    private String nickname;
+//    private String message;
 
+    //    public void init() {
+//        message = "Hello World!";
+//    }
     public void init() {
-        message = "Hello World!";
+        Dotenv dotenv = Dotenv.load();
+        nickname = dotenv.get("NICKNAME");
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -19,10 +25,12 @@ public class HelloServlet extends HttpServlet {
         // Hello
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
+//        out.println("<h1>" + message + "</h1>");
+        out.println("<h1>" + nickname + "</h1>");
         out.println("</body></html>");
     }
 
     public void destroy() {
     }
 }
+// 이름(name)과 경로(value, path)를 지정
